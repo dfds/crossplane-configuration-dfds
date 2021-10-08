e3af10f7545502835a5d4d00d095a1a03d04c2dd
Willi Carlsen <carlsenwilli@gmail.com>
2021-10-08 10:15:06 +0200

    WIP

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,9 @@
 
 # Replace the following variables with your docker repo and desired tag
 repo=dfdsdk/dfds-infra
-tag=myversion
+tag=wicar
+
+kubectl delete configuration.pkg dfdsdk-dfds-infra
 
 cd ../package
 rm -f *.xpkg
