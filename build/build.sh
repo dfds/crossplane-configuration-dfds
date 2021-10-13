32a1160416eb80f63d6fe863a0f75bbe479b29cd
Fisher Richard <rifis@DFDS.COM>
2021-10-13 14:33:08 +0100

    WIP - working example

--- build/build.sh
+++ build/build.sh
@@ -4,10 +4,11 @@
 
 # Replace the following variables with your docker repo and desired tag
 repo=dfdsdk/dfds-infra
-tag=myversion
+tag=rifis
 
 cd ../package
 rm -f *.xpkg
+kubectl delete configuration.pkg dfdsdk-dfds-infra
 kubectl crossplane build configuration
 kubectl crossplane push configuration $repo:$tag
 kubectl crossplane install configuration $repo:$tag
